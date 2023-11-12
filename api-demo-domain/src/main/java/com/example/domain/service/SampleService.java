7925d894a4d56c8ef742778243861197c4bc9f72
thrush1105 <thrush1105@gmail.com>
2023-11-12 14:22:51 +0900

    csvダウンロードAPIを作成

--- api-demo-domain/src/main/java/com/example/domain/service/SampleService.java
+++ api-demo-domain/src/main/java/com/example/domain/service/SampleService.java
@@ -1,12 +1,54 @@
 package com.example.domain.service;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.apache.ibatis.cursor.Cursor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.model.Sample;
+import com.example.domain.repository.SampleRepository;
 
 @Service
 public class SampleService {
 
+    private final SampleRepository sampleRepository;
+
+    public SampleService(SampleRepository sampleRepository) {
+        this.sampleRepository = sampleRepository;
+    }
+
     public String hello() {
         return "Hello!!";
+    }
+
+    @Transactional(readOnly = true)
+    public void outputCsv(String since, String until, PrintWriter writer)
+            throws IOException {
+        try (Cursor<Sample> samples = sampleRepository.findAll(since, until)) {
+            for (Sample sample : samples) {
+                outputCsvRow(sample, writer);
+            }
+        }
+    }
+
+    private void outputCsvRow(Sample sample, PrintWriter writer) {
+        StringBuffer sb = new StringBuffer();
+
+        sb.append(sample.getId());
+        sb.append(",");
+        sb.append(sample.getUuid());
+        sb.append(",");
+        sb.append(sample.getText());
+        sb.append(",");
+        sb.append(sample.getDate());
+        sb.append(",");
+        sb.append(sample.getCreatedAt());
+        sb.append(",");
+        sb.append(sample.getUpdatedAt());
+
+        writer.println(sb);
     }
 
 }
--- api-demo-web/src/main/java/com/example/app/SampleController.java
+++ api-demo-web/src/main/java/com/example/app/SampleController.java
@@ -1,10 +1,17 @@
 package com.example.app;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.domain.service.SampleService;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("sample")
@@ -19,6 +26,20 @@ public class SampleController {
     @GetMapping("hello")
     public String hello() {
         return sampleService.hello();
+    }
+
+    @GetMapping("csv")
+    public void index(
+            @RequestParam(name = "since", required = false) String since,
+            @RequestParam(name = "until", required = false) String until,
+            HttpServletResponse response)
+            throws IOException {
+        response.setContentType("text/csv");
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        PrintWriter writer = response.getWriter();
+
+        sampleService.outputCsv(since, until, writer);
     }
 
 }
